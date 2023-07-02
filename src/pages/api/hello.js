8c1db488709a47181f8ac3e2c74a878ea025a290
BryanBac <bebac@correo.url.edu.gt>
2023-07-01 21:07:54 -0600

    cambios y subida

---
+++ src/pages/api/hello.js
@@ -0,0 +1,13 @@
+import {createPool} from 'mysql2/promise';
+
+const db = createPool({
+  host: '3.17.56.89', // Cambia esto si tu base de datos está en un host diferente
+  user: 'root', // Reemplaza con el nombre de usuario de tu base de datos
+  password: 'Pass1234*', // Reemplaza con la contraseña de tu base de datos
+  database: 'clientes', // Reemplaza con el nombre de tu base de datos
+});
+
+export default async function handler(req, res) {
+  const [rows] = await db.query("Select * from clientes");
+  return res.status(200).json(rows)
+}
